5e8976915215032d4bb30aac35e3725dd621c84d
Haralan Dobrev <harry@hkdobrev.com>
2022-08-13 17:03:11 +0300

    Apply insecure option to alias setting as well

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -euxo pipefail
 
-mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
-
 insecure_option=""
 if ["$MINIO_INSECURE" == "true"]; then
   insecure_option="--insecure"
 fi
+
+mc alias set $insecure_option deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 
 mc mirror --overwrite $insecure_option $1 "deploy/$2"
