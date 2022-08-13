d8b3a5dadb1428cd04f37cae267c2bb531c76cf8
Haralan Dobrev <harry@hkdobrev.com>
2022-08-13 16:41:02 +0300

    Add an optional insecure option for minio

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,9 @@ set -euxo pipefail
 
 mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 
-mc mirror --overwrite $1 "deploy/$2"
+insecure_option=""
+if ["$MINIO_INSECURE" == "true"]; then
+  insecure_option="--insecure"
+fi
+
+mc mirror --overwrite $insecure_option $1 "deploy/$2"
