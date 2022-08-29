98438a5d45068824d87fc8938095ae86dabc05bd
Haralan Dobrev <harry@hkdobrev.com>
2022-08-29 20:45:26 +0300

    Do not pass empty argument if insecure is not set

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ if [[ "$MINIO_INSECURE" == "true" ]]; then
   insecure_option="--insecure"
 fi
 
-mc alias set "$insecure_option" deploy "$MINIO_ENDPOINT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+mc alias set ${insecure_option:+"$insecure_option"} deploy "$MINIO_ENDPOINT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
 
-mc mirror --overwrite "$insecure_option" $1 "deploy/$2"
+mc mirror --overwrite ${insecure_option:+"$insecure_option"} $1 "deploy/$2"
